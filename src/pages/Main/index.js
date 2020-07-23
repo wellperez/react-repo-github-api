0f02df9ee067a72f222ebe553e51ce0497aedcf1
Wellington Perez <wellingtonx3@hotmail.com>
2020-07-23 16:21:45 -0300

    Added style in main page

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,9 +1,24 @@
 import React from 'react';
 
-import { Title } from './style';
+import { FaGithubAlt, FaPlus } from 'react-icons/fa';
+import { Container, Form, SubmitButton } from './style';
 
 function Main() {
-  return <Title>Hello World!</Title>;
+  return (
+    <Container>
+      <h1>
+        <FaGithubAlt />
+        Repositórios
+      </h1>
+      <Form onSubmit={() => {}}>
+        <input type="text" placeholder="Adicionar Repositório" />
+
+        <SubmitButton type="submit">
+          <FaPlus color="#FFF" size={14} />
+        </SubmitButton>
+      </Form>
+    </Container>
+  );
 }
 
 export default Main;
